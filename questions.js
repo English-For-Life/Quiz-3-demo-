11b2ef12816d6db40e183a38b7041c6f742709a7
English-For-Life <83182532+English-For-Life@users.noreply.github.com>
2021-04-26 19:32:33 +0300

    Add files via upload

---
+++ questions.js
@@ -0,0 +1,28 @@
+//Array of objects
+const quiz = [
+    {
+        q:"Lucy ___ shopping every day.",
+        options:["goes", "is going", "go"],
+        answer: 0
+    },
+    {
+        q:"They ___ in the park when it started to rain heavily.",
+        options:["walked", "were walking", "was walking"],
+        answer: 1
+    },
+    {
+        q:"I ___ when my mum came home.",
+        options:["slept", "sleep", "was sleeping"],
+        answer: 2
+    },
+    {
+        q:"It's ___ (11.40)",
+        options:["twenty to twelve", "twenty past twelve", "fourty past eleven "],
+        answer: 0
+    },
+    {
+        q:"I haven't got ___ .",
+        options:["many money", "much money", "some money"],
+        answer: 1
+    }
+]
